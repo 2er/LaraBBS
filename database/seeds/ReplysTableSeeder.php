ffe3ca7041ed0a52096bed4085bb6f900ff518a8
wuchuanchuan <superwucc@gmail.com>
2018-08-07 10:58:04 +0000

    Add user replies

---
+++ database/seeds/ReplysTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Reply;
+use App\Models\Topic;
+use App\Models\User;
+
+class ReplysTableSeeder extends Seeder
+{
+    public function run()
+    {
+        $faker = app(\Faker\Generator::class);
+        $topic_ids = Topic::all()->pluck('id')->toArray();
+        $user_ids = User::all()->pluck('id')->toArray();
+
+        $replys = factory(Reply::class)->times(1000)->make()->each(function ($reply, $index) use ($topic_ids, $user_ids, $faker) {
+            $reply->topic_id = $faker->randomElement($topic_ids);
+            $reply->user_id = $faker->randomElement($user_ids);
+        });
+
+        Reply::insert($replys->toArray());
+    }
+
+}
+
---
+++ resources/views/topics/_reply_box.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wuchuanchuan
+ * Date: 2018/8/7
+ * Time: 下午6:16
+ */
