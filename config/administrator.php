be626ebf3255e9c1b6064c363971b025e8b33ffd
wuchuanchuan <superwucc@gmail.com>
2018-08-08 08:59:44 +0000

    Admin can manage content

--- config/administrator.php
+++ config/administrator.php
@@ -21,7 +21,7 @@ return array(
      *
      * @type string
      */
-    'title' => config('app.name'),
+    'title' => env('APP_NAME', 'Laravel'),
 
     /*
      * The path to your model config directory
@@ -58,7 +58,18 @@ return array(
      * 		'Analytics' => array('E-Commerce' => 'page.ecommerce.analytics'),
      *	)
      */
-    'menu' => [],
+    'menu' => [
+        '用户与权限' => [
+            'users',
+            'roles',
+            'permissions',
+        ],
+        '内容管理' => [
+            'categories',
+            'topics',
+            'replies',
+        ],
+    ],
 
     /*
      * The permission option is the highest-level authentication check that lets you define a closure that should return true if the current user
@@ -67,7 +78,7 @@ return array(
      * @type closure
      */
     'permission' => function () {
-        return Auth::check();
+        return Auth::check() && Auth::user()->can('manage_contents');
     },
 
     /*
@@ -90,7 +101,7 @@ return array(
      *
      * @type string
      */
-    'home_page' => '',
+    'home_page' => 'users',
 
     /*
      * The route to which the user will be taken when they click the "back to site" button
