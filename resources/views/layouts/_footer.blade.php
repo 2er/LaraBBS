063e6d4fb947e9d37c559b11e22596ac8c83b960
wuchuanchuan <superwucc@gmail.com>
2018-08-08 09:11:56 +0000

    Admin can manage site setting

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -4,6 +4,6 @@
             由 <a href="https://weibo.com/1726649233/profile?topnav=1&wvr=6" target="_blank">Lucien Wu</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
         </p>
 
-        <p class="pull-right"><a href="superwucc@gmail.com">联系我们</a></p>
+        <p class="pull-right"><a href="{{ setting('contact_email') }}">联系我们</a></p>
     </div>
 </footer>
